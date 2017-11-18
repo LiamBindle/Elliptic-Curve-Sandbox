7bf6e8ba8da3270a14b63c804e9ce354ab837184
Liam Bindle <lrb175@mail.usask.ca>
2017-11-17 19:03:51 -0600

    implemented prime fields

--- include/finite_fields.hpp
+++ include/finite_fields.hpp
@@ -3,19 +3,27 @@
 namespace ff {
     class Field {
     public:
-        virtual void add(const mpz_t& op1, const mpz_t& op2, mpz_t& result) = 0;
-        virtual void sub(const mpz_t& op1, const mpz_t& op2, mpz_t& result) = 0;
-        virtual void mul(const mpz_t& op1, const mpz_t& op2, mpz_t& result) = 0;
-        virtual void inv(const mpz_t& op1, const mpz_t& op2, mpz_t& result) = 0;
-        virtual void red(const mpz_t& op1, const mpz_t& op2, mpz_t& result) = 0;
+        virtual void add(const mpz_t op1, const mpz_t op2, mpz_t result) = 0;
+        virtual void sub(const mpz_t op1, const mpz_t op2, mpz_t result) = 0;
+        virtual void mul(const mpz_t op1, const mpz_t op2, mpz_t result) = 0;
+        virtual void inv(const mpz_t op1, mpz_t result) = 0;
+        virtual void red(const mpz_t op1, mpz_t result) = 0;
     };
 
     class PrimeField : Field {
     public:
-        virtual void add(const mpz_t& op1, const mpz_t& op2, mpz_t& result) override;
-        virtual void sub(const mpz_t& op1, const mpz_t& op2, mpz_t& result) override;
-        virtual void mul(const mpz_t& op1, const mpz_t& op2, mpz_t& result) override;
-        virtual void inv(const mpz_t& op1, const mpz_t& op2, mpz_t& result) override;
-        virtual void red(const mpz_t& op1, const mpz_t& op2, mpz_t& result) override;
+        virtual void add(const mpz_t op1, const mpz_t op2, mpz_t result) override;
+        virtual void sub(const mpz_t op1, const mpz_t op2, mpz_t result) override;
+        virtual void mul(const mpz_t op1, const mpz_t op2, mpz_t result) override;
+        virtual void inv(const mpz_t op1, mpz_t result) override;
+        virtual void red(const mpz_t op1, mpz_t result) override;
+
+        PrimeField(const mpz_t p) { mpz_init_set(this->p, p); }
+
+        const mpz_t& fp() { return p; }
+
+    protected:
+        mpz_t p;
+
     };
 }
--- src/finite_fields.cpp
+++ src/finite_fields.cpp
@@ -5,27 +5,23 @@ int tester() {
     return 1;
 }
 
-void ff::PrimeField::add(const mpz_t& op1, const mpz_t& op2, mpz_t& result) {
-    std::cout<<"called add\n";
-    return;
+void ff::PrimeField::add(const mpz_t op1, const mpz_t op2, mpz_t result) {
+    mpz_add(result, op1, op2);
 }
 
-void ff::PrimeField::sub(const mpz_t& op1, const mpz_t& op2, mpz_t& result) {
-    std::cout<<"called sub\n";
-    return;
+void ff::PrimeField::sub(const mpz_t op1, const mpz_t op2, mpz_t result) {
+    mpz_sub(result, op1, op2);
 }
 
-void ff::PrimeField::mul(const mpz_t& op1, const mpz_t& op2, mpz_t& result) {
-    std::cout<<"called mul\n";
-    return;
+void ff::PrimeField::mul(const mpz_t op1, const mpz_t op2, mpz_t result) {
+    mpz_mul(result, op1, op2);
 }
 
-void ff::PrimeField::inv(const mpz_t& op1, const mpz_t& op2, mpz_t& result) {
-    std::cout<<"called inv\n";
-    return;
+void ff::PrimeField::inv(const mpz_t op1, mpz_t result) {
+    mpz_invert(result, op1, p);
+
 }
 
-void ff::PrimeField::red(const mpz_t& op1, const mpz_t& op2, mpz_t& result) {
-    std::cout<<"called red\n";
-    return;
+void ff::PrimeField::red(const mpz_t op1, mpz_t result) {
+    mpz_mod(result, op1, p);
 }
